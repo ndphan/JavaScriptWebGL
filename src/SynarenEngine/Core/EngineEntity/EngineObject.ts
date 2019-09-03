7cf2c88eb071153e339bf95048ed819b89cf7320
Nam Phan <namp323@gmail.com>
2019-09-03 19:03:22 +1000

    add generated uid and interface for EngineObject

--- src/SynarenEngine/Core/EngineEntity/EngineObject.ts
+++ src/SynarenEngine/Core/EngineEntity/EngineObject.ts
@@ -2,12 +2,30 @@ import ModelPosition from "./ModelPosition";
 import EngineHelper from "../EngineHelper";
 import { EngineEvent } from "../Events";
 
-abstract class EngineObject extends ModelPosition {
+function generateId() {
+  return "xxxxxxxxx".replace(/[x]/g, function(c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+abstract class EngineObject extends ModelPosition implements EngineEntity {
   $physicsId: number;
+  $hidden: boolean = false;
+  $id: string = generateId();
   update(engineHelper: EngineHelper) {}
   render(engineHelper: EngineHelper) {}
   event(event: EngineEvent, engineHelper: EngineHelper): void {}
   init(engineHelper: EngineHelper) {}
+}
+
+export interface EngineEntity {
+  $id: string;
+  update(engineHelper: EngineHelper): void;
+  render(engineHelper: EngineHelper): void;
+  event(event: EngineEvent, engineHelper: EngineHelper): void;
+  init(engineHelper: EngineHelper): void;
 }
 
 export default EngineObject;
